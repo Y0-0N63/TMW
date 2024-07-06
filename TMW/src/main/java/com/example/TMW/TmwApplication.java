c5e43df7bd85893add5a64de1bdbb72ca53f62f1
최보윤 <boyun1625@naver.com>
2024-07-06 20:58:06 +0900

    first commit

---
+++ TMW/src/main/java/com/example/TMW/TmwApplication.java
@@ -0,0 +1,13 @@
+package com.example.TMW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmwApplication.class, args);
+	}
+
+}
---
+++ TMW/src/test/java/com/example/TMW/TmwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TMW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
