1b1ad21dec8473b2d4953796fc6c6f64f47cb027
은서 <162455166+eunseouo@users.noreply.github.com>
2024-09-26 10:33:43 +0900

    Update mainPage.js

--- MainPage/mainPage.js
+++ MainPage/mainPage.js
@@ -8,4 +8,20 @@ ifSuccessLogin.addEventListener("click", function success() {
   beforeLoginDiv.style.display = "none"; // "block"을 문자열로 처리
   afterLoginDiv.style.display = "block"; // "block"을 문자열로 처리
   signupP.style.display = "none";
+
+  
+  // 회원 정보 받아옴
+  const userData = {
+    name: "홍길동",
+    phone: "010-1234-5678",
+    address: "서울특별시 강남구",
+    career: "카페 - 3년",
+    healthState: "양호",
+    wishList: "운동 프로그램",
+    points: 1200,
+  };
+
+  // 상단 로그인 정보와 회원정보 영역에 이름을 설정
+  document.getElementById("TopUserName").textContent = userData.name;
 });
+
